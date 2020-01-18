124b3ed747ca9a0fed2269d41aae304c78481fb1
anytramm <z.martyna96@gmail.com>
2020-01-18 17:22:46 +0100

    countdown
    countdown

--- main.js
+++ main.js
@@ -1,19 +1,58 @@
-const countUserWin = 0;
-const countCompWin = 0;
-const countDraw = 0;
+const gamesStats = {
+    countUserWin: 0,
+    countCompWin: 0,
+    countDraw: 0,
+}
 
-const gameItems = ["rock", "paper", "scissors", "lizard", "spock"];
+const gameResult = {
+    userHand: "",
+    compHand: "",
+}
+
+const allHands = [...document.body.querySelectorAll(".choices__item--button")];
+allHands.forEach((hand, index) => allHands[index] = hand.getAttribute('data-option'));
+
+const userChoise = e => {
+    // gameResult.userHand = e.target;
+}
+
+function compChoise(hands) {
+    return allHands[Math.floor(Math.random * allHands.length)];
+}
 
 const modal = document.body.querySelector('.modal');
-const modalContent = document.body.querySelector('h4');
+const modalTitle = document.body.querySelector('h4');
 
-// const resultImage = document.body.querySelector('.results__image');
+const handBattle = (gameResult, gamesStats) =>{
+    if(userHand === compHand){
+        
+    }
+
+}
+
+const countdown = () => {
+    let countdown = 3;
+    modal.style.display = "flex";
+    modalTitle.textContent = (countdown).toString()
+    let interval = setInterval(function(){
+        modal.style.display = "flex";
+        modalTitle.textContent = (--countdown).toString()
+        if (countdown === 0) {
+            clearInterval(interval);
+        }
+    },1000);
+    handBattle(gameResult, gamesStats);
+}
+
+document.querySelector('.choices__item--button').addEventListener("click", countdown);
+
+
 
 // document.body.querySelector('#results__win').textContent = `You: ${countUserWin}`;
 // document.body.querySelector('#results__fail').textContent = `Computer: ${countCompWin}`;
 // document.body.querySelector('#results__draw').textContent = `Draw: ${countDraw}`;
 
-
+// const resultImage = document.body.querySelector('.results__image');
 // if(countUserWin > countCompWin){
 //     const iElement = document.createElement('i');
 //     iElement.classList="far fa-smile"
@@ -27,23 +66,3 @@ const modalContent = document.body.querySelector('h4');
 //     iElement.classList="far fa-meh"
 //     resultImage.appendChild(iElement);
 // }
-
-const computerMove = () => {
-    const random = Math.floor(Math.random() * 5);
-}
-
-
-const countdown = e => {
-    modal.style.display = "flex";
-    let countdown = 4;
-
-    while(countdown =! 0){
-        countdown--;
-        // modalContent.textContent = (--countdown).toString();
-    }
-    modal.style.display = "none";
-}
-
-document.querySelector('.choices__item--button').addEventListener("click" , countdown);
-
-
